a0019768baa33078a9b46c35c8af9e440e045f69
Rachel Hsia <rachel.hsia6546@gmail.com>
2020-09-25 17:48:36 +1000

    aiyah-

---
+++ frontend/src/Routes/Request/Components/RequestsHeader.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { RequestsSearch } from './RequestsSearch';
+
+class RequestsHeader extends Component {
+    render() {
+        return (
+            <div className="">
+                <h1>Requests heading in the requests header eheh</h1>
+            </div>
+        );
+    }
+}
+export default RequestsHeader;
