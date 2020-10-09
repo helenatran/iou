c487b6e4984d6aebf1b8e4ef872beb5b6b1bc80b
Rachel Hsia <rachel.hsia6546@gmail.com>
2020-10-10 00:49:23 +1100

    Added sinon for stubs and work in progress tests

--- backend/tests/testRequests.js
+++ backend/tests/testRequests.js
@@ -1,91 +1,121 @@
-// import { addNewRequest } from '../controllers/requestController.js';
-// import '../controllers/requestController.js';
-const controller = require('../controllers/requestController');
-// const addNewRequest = require('../controllers/requestController.js');
+const requestsController = require('../controllers/requestController');
+const userController = require('../controllers/users');
+const User = require('../models/userModel');
+
 const chai = require('chai');
-const { getAllRequests, addNewRequest } = require('../controllers/requestController');
+const expect = require('chai').expect;
+
 const assert = chai.assert;
+const sinon = require('sinon');
+// const mongoose = require('mongoose');
 
-let requests = [];
-let str = 'monkey';
+// let dummyUser1;
+// let dummyUser2;
 
-describe("testing tests", function () {
-	beforeEach(function () {
-		console.log('-----------beforeeach');
-		str = 'cat';
-	})
+// let req1;
+// let res1;
 
-	it("should be cat dog", function () {
-		str += "dog";
-		assert.equal(str, 'catdog')
-	});
-
-	it("should be cat bug", function () {
-		str += 'bug';
-		assert.equal(str, 'catbug');
+describe("scope", function() {
+	let num = 3;
+	it("testscope out of an it block", function() {
+		assert.equal(num, 3);
 	});
 });
 
-describe("Requests", function () {
 
-	describe("Create Requests", function () {
-		before(function setupDummyData() {
+describe("Test Requests", function() {
+	describe("Get requests", function() {
+		before(function() {
+			let getRequestsStub = sinon.stub(requestsController, 'getAllRequests').yields(null, null, JSON.stringify({ requests: ["request1", "request2", "request3"] }))
+		});
 
+		it("should add another object into the database", function(done) {
+			requestsController.getAllRequests()
+			.then((response) => {
+				expect(response.requests).to.equal(["request1", "request2", "request3"]);
+				doNotTrack();
+			})
+			.catch((err) => {
+				done(err);
+			})
+			console.log(testRequest);
+		});
+	});
+})
 
-		})
-		// test case for making request with valid data -> should return request
-		it("create requests with valid data should return the request just made", function () {
+// describe("Requests", function () {
+
+// 	describe("Create Requests", function () {
+// 		before(function setupDummyData() {
+
+// 			// // make users in the db
+// 			// dummyUser1 = new User();
+// 			// dummyUser2 = new User();
 			
-		})
-		// test case for making request with invalid data -> should return error
+// 			// let request = new Request();
 
-		// testing the types of all the fields
-	});
+// 			// //request data
+// 			// req1 = {
+// 			// 	"taskTitle": "dummyRequest",
+// 			// 	"taskDescription": "do this thing pls",
+// 			// 	"requestExpiry": "2020-10-25T11:50:00",
+// 			// 	"reward": [
+//             //         {rewarder: "alice", reward: "2 chocolates"}, 
+//             //         {rewarder: "bob", reward: "1 chocolate"}
+//             //     ]
+// 			// };
+			
+// 		});
 
-	describe("Read Requests", function () {
-		it("Get all requests", function () {
+// 		// test case for making request with valid data -> should return request
+// 		it("Create requests with valid data should return the request just made", function () {
+			
+// 			// let result = requestsController.addNewRequest(req, res);
 
-		});
+// 		})
+// 		// test case for making request with invalid data -> should return error
 
-		describe("Get requests by search keyword", function () {
-			// test case for one word -> requests contains the one word
-			// test case for two words -> requests contain the two words together
-			// test case for no search criteria -> returns all 
-			// test case for word that no requests have -> no requests return
-		});
+// 		// testing the types of all the fields
+// 	});
 
-		describe("Get requests by status", function () {
-			// test case for open requests
-			// test case for pending requests
-			// test case for closed requests
-		});
+// 	describe("Regarding Reading Requests", function () {
+// 		describe("Get all requests", function () {
+			
+// 			it("should return an array of 5 requests in a db collection with 5 documents", function() {
+// 				let allRequests = requestsController.getAllRequests({},{});
+// 				console.log('======================================HIT GET ALL REQUESTS TEST');
+// 				// console.log("HELLO" + allRequests);
+// 				// assert.equal(allRequests.length, 5)
+// 			})
 
-		describe("Get a user's request", function () {
-			// test case for a user that exists
-			// test case for user that doesnt exist
-		});
-	});
+// 		});
 
-	describe("Update Requests", function () {
+// 		describe("Get requests by search keyword", function () {
+// 		});
 
-		describe("Update title and description fields", function () {
-			//test case to edit title and description fields
-		});
+// 		describe("Get requests by status", function () {
+// 		});
 
-		describe("Update request status", function () {
+// 		describe("Get a user's request", function () {
+// 		});
+// 	});
+
+// 	describe("Update Requests", function () {
+
+// 		describe("Update title and description fields", function () {
+// 		});
+
+// 		describe("Update request status", function () {
 
 
-		});
+// 		});
 
-		describe("Request Rewards", function () {
+// 		describe("Request Rewards", function () {
 
-		});
-	});
+// 		});
+// 	});
 
-	describe("Delete Requests", function () {
+// 	describe("Delete Requests", function () {
 
-	});
-});
-
-console.log(str);
-console.log(getAllRequests());
+// 	});
+// });
