257652a9fe3ad09a053e8b760642e30bbdbc03c1
Rachel Hsia <rachel.hsia6546@gmail.com>
2020-10-10 01:01:15 +1100

    addNewRequest and getAllRequests api endpoints working

--- backend/controllers/requestController.js
+++ backend/controllers/requestController.js
@@ -1,28 +1,85 @@
 // requestController is for interfacing between the db and the frontend
 // import mongoose from 'mongoose';
 const mongoose = require('mongoose');
-const Request = require('../models/requestModel');
-
-// import {RequestSchema} from '../models/requestModel';
+const requestCollection = require('../models/requestModel');
 
 
-// const Request = mongoose.model('Request', RequestSchema);
 
-const addNewRequest = (req,res) => {
-    let newRequest = new Request(req.body);
+
+/**
+ * Create a request
+ * make post to endpoint in routes, including a request body:
+ * {
+ *      taskTitle: string,
+ *      taskDescription: string,
+ *      requesterId: objectID from user table,
+ *      status: "Open",
+ * }
+ */
+module.exports.addNewRequest = (req,res) => {
+    // need to add user IDs
+    let newRequest = new requestCollection(req.body);
+    console.log("request object", newRequest);
     
-    newRequest.save((err, Request) => {
-        if (err) {
-            res.send(err);
+    newRequest.save((err, requestObject) => {
+        if (ex) {
+            res.status(400).send(ex + " u did a fucky wucky :/"); 
         }
-        res.JSON(Request);
+        res.status(200).json(requestObject);
     });
 };
 
-const getAllRequests = (req,res) => {
-    Request.find({}).then((requests) => {
-        res.send(requests);
-    }) //todo add error catching
-}
+// Get a list of requests
+module.exports.getAllRequests = (req,res) => {
+    console.log("GET - all requests called");
+    requestCollection.find().then((requests) => {
+        res.status(200).send(requests);
+        console.log("GET - all requests returned");
+    }) 
+    .catch((ex) => {
+        res.status(400).json({ 'error': ex })
+    });
+};
 
-module.exports = { addNewRequest, getAllRequests };
+//Get request by ID
+module.exports.getARequest = (req, res) => {
+    console.log('hit get a req method');
+    const requestId = req.body.requestId;
+
+    console.log("GET - request " + requestId + " called");
+
+    requestCollection.findById(requestId, function(ex, request) {
+        res.status(200).send(request);
+        console.log("GET - request " + requestId + " returned");
+        console.log(request);    
+    })
+    .catch((ex) => {
+        res.status(400).json({ 'error': ex })
+    });
+};
+
+module.exports.SearchRequests = (req, res) => {
+    console.log("GET - search requests called")
+    //grab criteria from params
+
+    //db find with conditions, return that
+    // conditions: tasktitle, taskDecription or rewards contains keyword
+};
+
+module.exports.UpdateRequest = (req, res) => {
+    console.log("POST - update requests called")
+
+    //gets the id from body
+};
+
+module.exports.DeleteRequest = (req, res) => {
+    console.log("POST - delete requests called")
+
+    //get the id from body
+    
+    //check user id of request matches requester's user id
+
+
+};
+
+
--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,16 +1,21 @@
-// import * as requestController from '../controllers/requestController';
-
+const router = require('express').Router();
 const requestController = require('../controllers/requestController');
 
-const routes = (app) => {
-    app.route('/addNewRequest')
-        //POST endpoint for adding new request
-        .post(requestController.addNewRequest);
+// const routes = (app) => {
+//     app.route('/addNewRequest')
+//         //POST endpoint for adding new request
+//         .post(requestController.addNewRequest);
     
-    app.route('/')
+//     app.route('/')
 
-    app.route('/requests')
-        .get(requestController.getAllRequests);
-}
+//     app.route('/requests')
+//         .get(requestController.getAllRequests);
+// }
 
-module.exports = routes;
+
+router.get('/viewrequests', requestController.getAllRequests);
+router.get('/viewdetails/', requestController.getARequest);
+
+router.post('/add', requestController.addNewRequest);
+
+module.exports = router;
