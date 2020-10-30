a6609737296bdedc732afcfb6ce7f4e9d397364c
Jason <jasonko96@gmail.com>
2020-10-30 23:17:16 +1100

    Commented User Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,16 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
+/*
+ * Registers a user to the system
+ * POST request - receive these validated fields from userValidator:
+    "firstName":String,
+    "lastName":String,
+    "email" : String,
+    "password": "String",
+ */
 module.exports.registerUser = async (req, res) => {
     const salt = await bcrypt.genSalt(parseInt(process.env.SALT_ROUNDS));
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
@@ -13,33 +21,42 @@ module.exports.registerUser = async (req, res) => {
     });
 
     try {
-        const savedUser = await registeredUser.save();
+        await registeredUser.save();
         return res.status(200).json({
             message: 'User successfully registered'
         })
     } catch (error) {
         return res.status(500).json({
-            error: 'There was an issue with registering your user, please try again'
-        });
+            error: [{
+                error: 'Could not register your details, please try again'
+            }]});
     };
 }
 
+/*
+ * Allows user to login and authenticate to the system
+ * POST request - receive these validated fields from userValidator:
+    "email" : String,
+    "password": "String",
+ */
 module.exports.loginUser = async (req, res) => {
-    const user = await User.findOne({
-        email: req.body.email
-    });
+    const user = await User.findOne({ email: req.body.email });
+    
     if (!user)
         return res.status(400).json({
             error: [{
                 error: 'That email or password was incorrect'
             }]})
+
     const checkPassword = await bcrypt.compare(req.body.password, user.password);
+    
     if (!checkPassword)
         return res.status(400).json({
             error: [{
                 error: 'That email or password was incorrect'
             }]})
     
+    //Store the userID inside the JWT Payload for use on the front end
     const jwtPayload = {
         id: user._id,
     }
@@ -49,22 +66,28 @@ module.exports.loginUser = async (req, res) => {
     res.json({
         success: {
             token,
-            user: {
-                id: user._id
-            },
+            user: { id: user._id },
             message: 'Successful Authentication'
         }
     })
 }
 
-module.exports.validateToken = async (req, res) => {  
+/*
+ * Internal helper method for the front end, returns true if token is valid, false if token invalid
+ * POST request - receives this header from the front end:
+    "token": "String",
+ */
+module.exports.validateToken = async (req, res) => {
+
     const token = req.header("token");
+
     if (!token)
         return res.json(false);
+
     try {
         let verified = jwt.verify(token, process.env.JWT_SECRET);
-
         const user = await User.findById(verified.id);
+
         if (!user)
             return res.json(false);
         else
@@ -74,25 +97,39 @@ module.exports.validateToken = async (req, res) => {
     }
 }
 
+/*
+ * Internal helper method for the front end, tries to find an existing user
+ * GET request - receives this header from the front end:
+    "token": "String",
+ */
 module.exports.findUserByID = async (req, res) => {
     try {
         let encodedUserID = req.header("token");
         let decodedToken = jwt.verify(encodedUserID, process.env.JWT_SECRET)
+
         const user = await User.findById(decodedToken.id);
         if (!user)
             return res.status(404).json({
                 error: 'User could not be found'
             })
+
         return res.status(200).json({
             id: user._id,
         })
     } catch (error) {
         return res.status(500).json({
-            error: 'That user could not be found'
+            error: [{
+                error: 'That user could not be found'
+            }]
         })
     }
 }
 
+/*
+ * Internal helper method for the front end, returns the logged in users names
+ * POST request - receives this header from the front end:
+    "token": "String",
+ */
 module.exports.getUserName = async (req, res) => {
     try {
         let encodedUserID = req.header("token");
@@ -110,10 +147,19 @@ module.exports.getUserName = async (req, res) => {
     }
 }
 
+/*
+ * Retrieves entire user list from MongoDB
+ * GET request
+ */
 module.exports.getUsers = async (req, res) => {
     await User.find()
         .then((users) => {
             res.status(200).send(users);
         })
-        .catch(err => res.status(400).json({ 'error': err }));
+        .catch(err => {
+            res.status(400).json({ 
+                error: [{
+                    error: 'Could not retrieve users'
+                }]})
+        })
 } 
